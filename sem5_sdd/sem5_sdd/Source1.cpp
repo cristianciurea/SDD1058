fe7ffeb941261599efaad147abeb3c43d6b818cd
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-26 22:55:42 +0200

    update

--- sem5_sdd/sem5_sdd/Source1.cpp
+++ sem5_sdd/sem5_sdd/Source1.cpp
@@ -55,3 +55,83 @@ void inserareLP(nodLP **capLP, nodLS *capLS)
 		temp->next = nou;
 	}
 }
+
+void traversareLS(nodLS *cap)
+{
+	nodLS *temp = cap;
+	while(temp)
+	{
+		printf("\nVarsta=%d, Nume=%s, Medie=%5.2f", temp->inf.varsta, temp->inf.nume, temp->inf.medie);
+		temp = temp->next;
+	}
+}
+
+void traversareLP(nodLP *cap)
+{
+	nodLP* temp = cap;
+	while(temp)
+	{
+		printf("\nSublista: ");
+		traversareLS(temp->inf);
+		temp = temp->next;
+	}
+}
+
+void dezalocareLS(nodLS *cap)
+{
+	nodLS *temp = cap;
+	while(temp)
+	{
+		nodLS *temp2 = temp->next;
+		free(temp->inf.nume);
+		free(temp);
+		temp = temp2;
+	}
+}
+
+void dezalocareLP(nodLP *cap)
+{
+	nodLP* temp = cap;
+	while(temp)
+	{
+		nodLP *temp2 = temp->next;
+		dezalocareLS(temp->inf);
+		temp = temp2;
+	}
+}
+
+void main()
+{
+	int n;
+	printf("Nr. studenti=");
+	scanf("%d", &n);
+	student s;
+	char buffer[20];
+	nodLP *capLP = NULL;
+	nodLS *capLSpar = NULL, *capLSimpar = NULL;
+
+	for(int i=0;i<n;i++)
+	{
+		printf("\nVarsta=");
+		scanf("%d", &s.varsta);
+		printf("\nNume=");
+		scanf("%s", buffer);
+		s.nume = (char*)malloc((strlen(buffer)+1)*sizeof(char));
+		strcpy(s.nume, buffer);
+		printf("\nMedie=");
+		scanf("%f", &s.medie);
+
+		if(s.varsta % 2 ==0)
+			inserareLS(&capLSpar, s);
+		else
+			inserareLS(&capLSimpar, s);
+
+	}
+
+	inserareLP(&capLP, capLSpar);
+	inserareLP(&capLP, capLSimpar);
+
+	traversareLP(capLP);
+	dezalocareLP(capLP);
+	
+}
